7323885b7cc1efaf72c5b7ebc0486f006b1edb5d
Alex <alex.lawrence@pau.edu.ng>
2026-09-10 17:43:00 +0100

    Add WEEK-3 assignments

---
+++ WEEK-3/practice_5/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let special_character = '@';
+    let alphabet: char = 'B';
+    let surname = "Lawrence";
+    let first_name = "Alex";
+    let middle_name = "Victor";
+
+
+    println!(" ");
+    println!("Special character: {}", special_character);
+    println!("Alphabet: {}", alphabet);
+    println!("Name: {} {} {}", surname, middle_name, first_name);
+}
---
+++ WEEK-3/practice_6/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let interest: f32 = 8; //integer assigned to a float
+    println!("Interest is {}", interest);
+}
